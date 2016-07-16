d5b9f5108927ec22b74728d3c7bb8cb1d6ce9c8b
Ian D. Bollinger <ian.bollinger@gmail.com>
2016-07-15 21:52:15 -0400

    Document Place.purs.

--- src/Skald/Place.purs
+++ src/Skald/Place.purs
@@ -4,6 +4,8 @@
 -- http://opensource.org/licenses/MIT>. This file may not be copied, modified,
 -- or distributed except according to those terms.
 
+-- | Places are discrete areas of the world containing objects the player can
+-- directly interact with.
 module Skald.Place (
     -- TODO: don't export innards.
     module Skald.Internal,
@@ -38,13 +40,16 @@ import Skald.Object as Object
 import Skald.Object (Object)
 import Skald.Internal (Place (..), Exits (..), Objects (..))
 
+-- | An empty collection of exits.
 noExits :: Exits
 noExits = Exits (StrMap.empty)
 
+-- | An empty collection of objects.
 noObjects :: Objects
 noObjects = Objects (StrMap.empty)
 
 -- TODO: fully implement.
+-- | Create a string representation of the given place for debugging purposes.
 toString :: Place -> String
 toString place' =
     "Place\n\
@@ -72,46 +77,58 @@ name (Place place') = place'.name
 description :: Place -> String
 description (Place place') = place'.describer (Place place')
 
+-- | The objects contained within the given place.
 objects :: Place -> Objects
 objects (Place place') = place'.objects
 
+-- | The object contained within the given place with the given name, if it
+-- exists.
 object :: String -> Place -> Maybe Object
 object name' (Place place') = case place'.objects of
     Objects objects' -> StrMap.lookup name' objects'
 
+-- | The exits leading out of the given place.
 exits :: Place -> Exits
 exits (Place place') = place'.exits
 
+-- | The list of directions the place may be exited from.
 exitDirections :: Place -> List Direction
 exitDirections (Place { exits: Exits exits' }) =
     List.fromFoldable (StrMap.keys exits')
 
--- | `exitName direction place`
+-- | Adds an exit to the given place via the given direction.
 exitName :: String -> Place -> Maybe String
 exitName exit (Place { exits: Exits exits' }) = StrMap.lookup exit exits'
 
+-- | Whether a place has been visited by the player.
 visited :: Place -> Boolean
 visited (Place place') = place'.visited
 
+-- | Whether a place has not been visited by the player.
 unvisited :: Place -> Boolean
 unvisited (Place place') = not place'.visited
 
+-- | Set whether a place has been visited by the player.
 setVisited :: Boolean -> Place -> Place
 setVisited visited' (Place place') = Place (place' { visited = visited' })
 
+-- | Modify the given place's contained objects with the given function.
 updateObjects :: (Objects -> Objects) -> Place -> Place
 updateObjects f (Place place') = Place (place' { objects = f place'.objects })
 
+-- | Remove an object from the given place.
 removeObject :: Object -> Place -> Place
 removeObject object' =
     updateObjects
         (\(Objects x) -> Objects (StrMap.delete (Object.name object') x))
 
+-- | Add an object to the given place.
 addObject :: Object -> Place -> Place
 addObject object' =
     updateObjects
         (\(Objects x) -> Objects (StrMap.insert (Object.name object') object' x))
 
+-- | The list of names of objects contained in the given place.
 objectNames :: Place -> List String
 objectNames (Place { objects: Objects objects' }) =
     List.fromFoldable (StrMap.keys objects')
